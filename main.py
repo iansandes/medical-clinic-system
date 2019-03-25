8a16ff134943e232c4f397b74f782ecb9173c9c2
Ian Sandes <iansandes15@gmail.com>
2019-03-25 15:35:50 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print(20 * "-=")
+print("       SISTEMA DE CLÍNICA MÉDICA")
+print(20 * "-=")
+
+print('''--------------------
+1 - Pacientes
+2 - Médicos
+3 - Consultas
+4 - Administrativo
+--------------------''')
+
+atendimento = int(input("Escolha o tipo de atendimento a ser usado: "))
+
+if atendimento == 1:
+    from pacientes import *
+    menu()
