a12d5a0d0ddf7a03fe80de0b83a2df1ce935e44b
Ian Sandes <iansandes15@gmail.com>
2019-03-26 21:59:20 -0300

    fix bugs

--- pacientes.py
+++ pacientes.py
@@ -1,42 +1,45 @@
+import json
+
+
+
 def cadastro():
         dados = {}
         dados['name'] = input('Nome: ')
-        dados['email'] = input('Email:')
-        dados['phone'] = input('Telefone: ')
-        dados['rg'] = input('RG: ')
-        dados['cpf'] = input('CPF: ')
-        dados['number'] = input('Endereço: ')
-        dados['address'] = input('número: ')
-        dados['hood'] = input('Bairro: ')
-        dados['city'] = input('Cidade: ')
-        dados['estado'] = input('UF: ')
-        dados['plano'] = input('Plano de saúde: ')
+    #    dados['email'] = input('Email:')
+    #   dados['phone'] = input('Telefone: ')
+    #    dados['rg'] = input('RG: ')
+    #    dados['cpf'] = input('CPF: ')
+    #    dados['number'] = input('Endereço: ')
+    #    dados['address'] = input('número: ')
+    #    dados['hood'] = input('Bairro: ')
+    #    dados['city'] = input('Cidade: ')
+    #    dados['estado'] = input('UF: ')
+    #    dados['plano'] = input('Plano de saúde: ')
         return dados
 
 
 def listar_pacientes():
-    import pickle
-    lista_pacientes = pickle.load(open('dados_pacientes.txt', 'rb'))
+    lista_pacientes = json.load(open('dados_pacientes.json', 'r'))
     print(lista_pacientes)
 
 def menu():
-    import pickle
-    dados_pacientes = []
     while True:
         print('''1 - Cadastrar novo paciente
-        2 - Listar pacientes
-        3 - Sair
+2 - Listar pacientes
+3 - Sair
         ''')
         opcao = int(input('Selecione uma opção: '))
         if opcao == 1:
-            pickle.dump(cadastro(), open('dados_pacientes.txt', 'wb'))
+            pacientes = json.loads(open('dados_pacientes.json', 'r').readlines())
+            with open('dados_pacientes.json', mode='w') as dados_pacientes:
+                pacientes.append(cadastro())
+                x = json.dumps(pacientes)
+                dados_pacientes.write(x)
+
+
         elif opcao == 2:
             listar_pacientes()
         elif opcao == 3:
             break
         else:
             print("Opção inválida!")
-
-
-
-
